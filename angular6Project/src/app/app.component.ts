38ea766ae92d0b2a6b8490a7078d5a88cab0d4b3
albert18 <caps18@hotmail.com>
2018-05-24 13:37:00 -0400

    Property binding

--- angular6Project/src/app/app.component.ts
+++ angular6Project/src/app/app.component.ts
@@ -4,7 +4,12 @@ import { AST } from '@angular/compiler';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color:orange;
+    }
+  `]
 })
 export class AppComponent {
 
--- angular6Project/src/app/servers/servers.component.ts
+++ angular6Project/src/app/servers/servers.component.ts
@@ -2,14 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '.app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'NO server was created';
+  serverName = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server Was Created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
